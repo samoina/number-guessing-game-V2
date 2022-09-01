4fb9dab588270603931055581ae563a22757b43d
Samoina <samoina.w@gmail.com>
2022-09-01 11:01:27 +0300

    Add index file, include game rules on JS comments and add UI variables

---
+++ main.js
@@ -0,0 +1,26 @@
+//Player must guess a number between minimum and maximum
+//Player gets a certain number of guesses
+//Let player know how many guesses remain
+//Let player know the correct answer if they lose
+//Let player play again
+
+
+//UI variables 
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessInput = document.querySelector('#guess-input'),
+      paraMessage = document.querySelector('.message'),
+      submitBtn = document.querySelector('#guess-value');
+
+let minimumNumber,
+    maximumNumber;
+
+
+
+
+
+
+
+
+
