45fed59710287a292dd167dff4c128b963df9415
Samoina <samoina.w@gmail.com>
2022-09-05 09:38:48 +0300

    Add functionality to reload the page after the game is over

--- main.js
+++ main.js
@@ -4,8 +4,7 @@
 //Let player know the correct answer if they lose
 //Let player play again
 
-
-//UI variables 
+//UI variables
 const game = document.querySelector('#game'),
       minNum = parseInt(document.querySelector('.min-num').innerHTML),
       maxNum = parseInt(document.querySelector('.max-num').innerHTML),
@@ -16,14 +15,16 @@ const game = document.querySelector('#game'),
 
 newGameBtn.style.display = 'none';
 
-let correctGuess = Math.floor(Math.random() * 100) + 1,
+let correctGuess = generateRandomNum(),
     counter = 4;
-    
+
+function generateRandomNum(){
+  return Math.floor(Math.random() * 100) + 1
+}
 
 console.log(correctGuess);
 
 guessInput.focus();
-
 
  //Generate the random min and max numbers
 //  function generateNumbers(){
@@ -38,7 +39,6 @@ guessInput.focus();
 //  }
 
 // generateNumbers();
-
 
 //Function to show response and feedback
 function showResponse(message, color){
@@ -56,53 +56,118 @@ function resetGame(){
   newGameBtn.addEventListener('click', startNewGame);
 
   function startNewGame(ev){
-    newGameBtn.style.display = 'none';
-    guessInput.value = '';
-    guessInput.disabled = false;
-    submitBtn.disabled = false;
+    // newGameBtn.style.display = 'none';
+    // guessInput.value = '';
+    // guessInput.disabled = false;
+    // submitBtn.disabled = false;
 
-    counter = 4;
-    paraMessage.textContent ='';
-    correctGuess = Math.floor(Math.random() * 100) + 1;
+    // counter = 4;
+    // paraMessage.textContent ='';
+    // correctGuess = Math.floor(Math.random() * 100) + 1;
+    window.location.reload();
 
     ev.preventDefault();
   }
-  
- 
-  
 
 }
 
 submitBtn.addEventListener('click', checkGuess);
 function checkGuess(ev){
   let userGuess = parseInt(guessInput.value);
-      
 
-    if(userGuess === correctGuess) {
+    if(isNaN(userGuess) || userGuess < minNum || userGuess > maxNum) {
+      showResponse(`Please include a number within the limits`, 'Red')
+    } else if (userGuess === correctGuess){
       showResponse('Congrats! you got that right!', 'Green');
       resetGame();
     } else {
-      
-      if(userGuess<correctGuess){
-        counter--;
-        showResponse(`Wrong. Your guess is too low. You have ${counter} more guesses`, 'Red')
-      } else if (userGuess>correctGuess){
-        counter--;
-        showResponse(`Wrong. Your guess is too high. You have ${counter} more guesses`, 'Red')
+      counter--;
+      if(userGuess<correctGuess) {
+        showResponse(`Wrong. Your guess is too low. You have ${counter} more guesses`, 'Red');
+      } else {
+        showResponse(`Wrong. Your guess is too high. You have ${counter} more guesses`, 'Red');
       }
     }
 
-    if(counter===0 && userGuess !== correctGuess) {
+     if(counter===0 && userGuess !== correctGuess) {
       showResponse(`!!!Game Over!!! The correct answer was ${correctGuess}`, 'Red');
       resetGame();
     }
-
-  
   ev.preventDefault();
 }
 
+//BRAD's METHOD on the udemy course
+//create the variables
+// let min = 15,
+//   max = 20,
+//   winningNum = getRandomNum(min, max),
+//   guessesLeft = 3;
+ 
+// //UI elements
+// const game = document.querySelector("#game"),
+//   minNum = document.querySelector(".min-num"),
+//   maxNum = document.querySelector(".max-num"),
+//   guessBtn = document.querySelector("#guess-value"),
+//   guessInput = document.querySelector("#guess-input"),
+//   message = document.querySelector(".message");
 
+// //Assign UI min and max
+// minNum.textContent = min;
+// maxNum.textContent = max;
 
+// //play again eventL but use event delegation
+// game.addEventListener('mousedown', function(e){
+//   if(e.target.className === 'play-again'){
+//     window.location.reload()
+//   }
+// })
 
+// //add eL
+// guessBtn.addEventListener("click", checkGuess);
 
+// function checkGuess(ev) {
+//   let guess = parseInt(guessInput.value);
 
+//   if (isNaN(guess) || guess < min || guess > max) {
+//     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+//   }
+
+//   //check if won
+//   if (guess === winningNum) {
+//     gameOver(true, `Congratulations, you win! ${winningNum} is correct`)
+//   } else {
+//     guessesLeft -= 1;
+//     if (guessesLeft === 0) {
+//       gameOver(false, `Game over! You lost. The correct number was ${winningNum}`)
+//     } else {
+//       setMessage(`Guess is not correct. ${guessesLeft} guesses remaining`, 'red');
+//       guessInput.style.borderColor = 'red';
+//       guessInput.value = '';
+//     }
+//   }
+
+//   ev.preventDefault();
+// }
+
+// function setMessage(msg, color) {
+//   message.textContent = msg;
+//   message.style.color = color;  
+// }
+
+// function gameOver(won, msg) {
+//   let colour;
+//   won === true ? colour = 'green' : colour = 'red'
+//   guessInput.disabled = true;
+//   guessInput.style.borderColor = colour;
+//   message.style.color = colour;
+//   setMessage(msg);
+
+//   //play again
+//   guessBtn.value = 'Play Again';
+//   guessBtn.className += 'play-again';  
+// }
+
+// //Get the winning number
+// function getRandomNum(min, max){
+//   return Math.floor(Math.random() * ((max-min)+1)+ min);  
+// }
